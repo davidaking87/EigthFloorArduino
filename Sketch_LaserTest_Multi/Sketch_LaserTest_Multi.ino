5fdd60cf08ccd6753aeac50b882e36df855d9541
Kevin <nivekyang@yahoo.com>
2017-04-07 00:38:25 -0600

    Finished Multi-Laser Test
    Works on my Arduino Uno. (SDA = A5 & SCL = A4)

--- Sketch_LaserTest_Multi/Sketch_LaserTest_Multi.ino
+++ Sketch_LaserTest_Multi/Sketch_LaserTest_Multi.ino
@@ -1,35 +1,49 @@
-// Site: http://arduino.stackexchange.com/questions/25050/multiple-vl6180x-distance-sensors-interactive-problem
+// inspiration from site: http://arduino.stackexchange.com/questions/25050/multiple-vl6180x-distance-sensors-interactive-problem
+
+/* Multiple laser procedure:
+ *  1. Wire SHDN (SHutDowN) pins.
+ *  2. Disable ALL lasers. (write SHDN pin > LOW)
+ *  3. Enable 1 laser. (write SHDN pin > HIGH & call laser.begin())
+ *  4. Read value.
+ */
+/* Note: 
+ *  For Arduino Uno, SDA = A5 & SCL = A4
+ *      for other boards, see: https://www.arduino.cc/en/Reference/Wire
+ */
 
 #include "Adafruit_VL6180X.h"
 
-Adafruit_VL6180X laser1;
-  byte laser1SHDNPin = 1; // SHutDowN pin
-  #define LASER1_ADDRESS 0x30;
-Adafruit_VL6180X laser2;
-  byte laser2SHDNPin = 2;
-Adafruit_VL6180X laser3;
-  byte laser3SHDNPin = 3;
+Adafruit_VL6180X lasers;
+  byte laser1SHDNPin = 7; // SHutDowN pin
+  byte laser2SHDNPin = 6;
+//  byte laser3SHDNPin = 3;
 
 void setup() {
   pinMode(laser1SHDNPin, OUTPUT);
-//  pinMode(laser2SHDNPin, OUTPUT);
+  pinMode(laser2SHDNPin, OUTPUT);
 //  pinMode(laser3SHDNPin, OUTPUT);
-  digitalWrite(laser1SHDNPin, HIGH); // Disable laser
-//  digitalWrite(laser2SHDNPin, HIGH); // Disable laser
+  digitalWrite(laser1SHDNPin, LOW); // Disable laser1
+  digitalWrite(laser2SHDNPin, LOW); // Disable laser2
 //  digitalWrite(laser3SHDNPin, HIGH); // Disable laser
-  
-  digitalWrite(laser1Pin, LOW);
-  laser1.begin();
-  laser1.setAddress(LASER1_ADDRESS);
-  
-  laser2.begin();
-  laser3.begin();
-  sensor.begin();
+
   Serial.begin(9600);
 }
 
 void loop() {
-//  Serial.println(sensor.readReg(0x212),HEX);
-  Serial.println(sensor.readRange());
+  // Enable laser 1
+  digitalWrite(laser2SHDNPin, LOW);  // Disable laser2 before...
+  digitalWrite(laser1SHDNPin, HIGH); // enabling laser1
+  // Print laser 1 value
+  Serial.print("laser1: ");
+  lasers.begin();
+  Serial.print(lasers.readRange());
+
+  // Enable laser 2
+  digitalWrite(laser1SHDNPin, LOW);  // Disable laser1 before...
+  digitalWrite(laser2SHDNPin, HIGH); // enabling laser2
+  // Print laser 2 value
+  Serial.print("\t\tlaser2: ");
+  lasers.begin();
+  Serial.println(lasers.readRange());
   delay(500);
 }
